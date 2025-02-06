ddb30253037129a9b351595f35385abce4df62b4
Sapan <sapanashara26@gmail.com>
2025-02-06 16:35:57 +0530

    init

---
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mainapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mainapp.Activity.ActivityTasks
+import com.example.mainapp.Drawables.DrawablesTasks
+import com.example.mainapp.LayoutViews.LayoutTasks
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val ButtonOne: Button= findViewById(R.id.activity)
+        ButtonOne.setOnClickListener {
+            val intent1 = Intent(this, ActivityTasks::class.java)
+            startActivity(intent1)
+        }
+        val ButtonTwo: Button= findViewById(R.id.layout_views)
+        ButtonTwo.setOnClickListener {
+
+            val intent2 = Intent(this, LayoutTasks::class.java)
+            startActivity(intent2)
+        }
+        val ButtonThree: Button= findViewById(R.id.drawables)
+        ButtonThree.setOnClickListener {
+
+            val intent3 = Intent(this, DrawablesTasks::class.java)
+            startActivity(intent3)
+        }
+    }
+}
