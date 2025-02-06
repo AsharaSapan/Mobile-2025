7650b29bc793845104fbd45c11d8a5c29f32d2f2
Sapan <101036448+AsharaSapan@users.noreply.github.com>
2025-02-06 17:35:31 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/mainapp/MainActivity.kt
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -8,8 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mainapp.Activity.ActivityTasks
-import com.example.mainapp.Drawables.DrawablesTasks
-import com.example.mainapp.LayoutViews.LayoutTasks
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
