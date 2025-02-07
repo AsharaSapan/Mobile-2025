232dd28a8341aef0cf901c2a3e3711216fdeb837
Sapan <sapanashara26@gmail.com>
2025-02-07 09:57:41 +0530

    Changed the missing import code

--- app/src/main/java/com/example/mainapp/Activity/ActivityTasks.kt
+++ app/src/main/java/com/example/mainapp/Activity/ActivityTasks.kt
@@ -3,11 +3,7 @@ package com.example.mainapp.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.mainapp.LayoutViews.LayoutTasks
 import com.example.mainapp.R
 
 class ActivityTasks : AppCompatActivity() {
