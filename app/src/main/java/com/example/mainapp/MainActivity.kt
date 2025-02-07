94c1736d854f3912cf6f1c42b4136a53634bbaae
Sapan <sapanashara26@gmail.com>
2025-02-07 09:12:03 +0530

    drawables task completed

--- app/src/main/java/com/example/mainapp/MainActivity.kt
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -7,14 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.mainapp.Activity.ActivityTasks
 import com.example.mainapp.Drawables.DrawablesTasks
-import com.example.mainapp.LayoutViews.LayoutTasks
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        val ButtonThree: Button= findViewById(R.id.drawables)
+        ButtonThree.setOnClickListener {
+
+            val intent3 = Intent(this, DrawablesTasks::class.java)
+            startActivity(intent3)
+        }
     }
 }
