b7cf0f095774e9e376ebccab8d1c71eaddf2c21c
Sapan <sapanashara26@gmail.com>
2025-02-07 10:06:33 +0530

    Added the missing drawable files

--- app/src/main/java/com/example/mainapp/MainActivity.kt
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -1,15 +1,7 @@
 package com.example.mainapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.mainapp.Activity.ActivityTasks
-import com.example.mainapp.Drawables.DrawablesTasks
-import com.example.mainapp.LayoutViews.LayoutTasks
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
