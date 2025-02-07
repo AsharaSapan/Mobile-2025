fc6f7bb3afb1a0943a0230caeb9aacaac6473013
Sapan <sapanashara26@gmail.com>
2025-02-07 12:03:54 +0530

    task drawables selector completed

--- app/src/main/java/com/example/mainapp/MainActivity.kt
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mainapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mainapp.Selector_Tasks.DrawableSelectorTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        val buttonOne:Button = findViewById(R.id.selector)
+        buttonOne.setOnClickListener {
+            val intent4=Intent(this,DrawableSelectorTask ::class.java)
+            startActivity(intent4)
+        }
     }
 }
