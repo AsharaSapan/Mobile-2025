7fbe0bd43d585cbc88fcc22a22f7964b114eaf8e
Sapan <sapanashara26@gmail.com>
2025-02-10 14:23:14 +0530

    dimension for responsive design completed

--- app/src/main/java/com/example/mainapp/MainActivity.kt
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mainapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        val buttonFive:Button=findViewById(R.id.dimension)
+        buttonFive.setOnClickListener {
+            val intent5=Intent(this,DimensionTask::class.java)
+            startActivity(intent5)
+        }
     }
 }
