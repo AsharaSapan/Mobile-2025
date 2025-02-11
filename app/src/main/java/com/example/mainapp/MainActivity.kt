ac48b47957a00785c09bc105506e6501a0e252ef
Sapan <sapanashara26@gmail.com>
2025-02-11 18:50:33 +0530

    changes

--- app/src/main/java/com/example/mainapp/MainActivity.kt
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -17,34 +17,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dimensionButton:Button=findViewById(R.id.dimension)
+        val dimensionButton: Button = findViewById(R.id.dimension)
         dimensionButton.setOnClickListener {
-            val intent5=Intent(this,DimensionTask::class.java)
+            val intent5 = Intent(this, DimensionTask::class.java)
             startActivity(intent5)
-            
-        val selectorButton:Button = findViewById(R.id.selector)
-        selectorButton.setOnClickListener {
-            val intent4=Intent(this,DrawableSelectorTask ::class.java)
-            startActivity(intent4)
-        }
-        
-        val buttonOne: Button = findViewById(R.id.activity)
-        buttonOne.setOnClickListener {
-            val intent1 = Intent(this, ActivityTasks::class.java)
-            startActivity(intent1)
-        }
 
-        val buttonTwo: Button= findViewById(R.id.layout_views)
-        buttonTwo.setOnClickListener {
-            val intent2 = Intent(this, LayoutTasks::class.java)
-            startActivity(intent2)
-        }
+            val selectorButton: Button = findViewById(R.id.selector)
+            selectorButton.setOnClickListener {
+                val intent4 = Intent(this, DrawableSelectorTask::class.java)
+                startActivity(intent4)
+            }
 
-        val buttonThree: Button= findViewById(R.id.drawables)
-        buttonThree.setOnClickListener {
-            val intent3 = Intent(this, DrawablesTasks::class.java)
-            startActivity(intent3)
-        }
+            val buttonOne: Button = findViewById(R.id.activity)
+            buttonOne.setOnClickListener {
+                val intent1 = Intent(this, ActivityTasks::class.java)
+                startActivity(intent1)
+            }
 
+            val buttonTwo: Button = findViewById(R.id.layout_views)
+            buttonTwo.setOnClickListener {
+                val intent2 = Intent(this, LayoutTasks::class.java)
+                startActivity(intent2)
+            }
+
+            val buttonThree: Button = findViewById(R.id.drawables)
+            buttonThree.setOnClickListener {
+                val intent3 = Intent(this, DrawablesTasks::class.java)
+                startActivity(intent3)
+            }
+
+        }
     }
 }
