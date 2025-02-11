4310da1a1de017dda258865546b28a00daf5ee32
Sapan <101036448+AsharaSapan@users.noreply.github.com>
2025-02-11 18:38:49 +0530

    Updated MainActivity.kt

--- app/src/main/java/com/example/mainapp/MainActivity.kt
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -14,22 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val ButtonThree: Button= findViewById(R.id.drawables)
-        ButtonThree.setOnClickListener {
 
-            val intent3 = Intent(this, DrawablesTasks::class.java)
-            startActivity(intent3)
-        val ButtonTwo: Button= findViewById(R.id.layout_views)
-        ButtonTwo.setOnClickListener {
+        val buttonOne: Button = findViewById(R.id.activity)
+        buttonOne.setOnClickListener {
+            val intent1 = Intent(this, ActivityTasks::class.java)
+            startActivity(intent1)
+        }
 
+        val buttonTwo: Button= findViewById(R.id.layout_views)
+        buttonTwo.setOnClickListener {
             val intent2 = Intent(this, LayoutTasks::class.java)
             startActivity(intent2)
         }
 
-        val ButtonOne: Button = findViewById(R.id.activity)
-        ButtonOne.setOnClickListener {
-            val intent1 = Intent(this, ActivityTasks::class.java)
-            startActivity(intent1)
+        val buttonThree: Button= findViewById(R.id.drawables)
+        buttonThree.setOnClickListener {
+            val intent3 = Intent(this, DrawablesTasks::class.java)
+            startActivity(intent3)
         }
+
     }
 }
