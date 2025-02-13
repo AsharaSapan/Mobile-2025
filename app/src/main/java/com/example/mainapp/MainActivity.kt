b3d980ea2f1a7213e8b0bd880922f8ba315aad88
Sapan <sapanashara26@gmail.com>
2025-02-13 13:08:07 +0530

    dialogs task completed

--- app/src/main/java/com/example/mainapp/MainActivity.kt
+++ app/src/main/java/com/example/mainapp/MainActivity.kt
@@ -8,11 +8,18 @@ import com.example.mainapp.Selector_Tasks.DrawableSelectorTask
 import com.example.mainapp.LayoutViews.LayoutTasks
 import com.example.mainapp.Drawables.DrawablesTasks
 import com.example.mainapp.Activity.ActivityTasks
+import com.example.mainapp.Dialogs.DialogsTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonDialog: Button = findViewById(R.id.dialogs)
+        buttonDialog.setOnClickListener {
+            val intent6 = Intent(this, DialogsTask::class.java)
+            startActivity(intent6)
+        }
 
         val buttonSix: Button = findViewById(R.id.viewpager)
         buttonSix.setOnClickListener {
